1b766651f7261e45cb0ba12b4eb8feed5a882b73
Diogo <diogopalma@live.com>
2024-09-30 16:29:08 -0400

    changes docker

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import api from "./infrastructure/entrypoint/api";
 import MongoClient from './infrastructure/database/mongo';
 import path from "path";
 
-(async function() {  
+(async function() {    
   MongoClient.connect();
   global.appRoot = path.resolve(__dirname, '..');
   const port = process.env.PORT || 9000;
--- src/infrastructure/database/mongo/index.ts
+++ src/infrastructure/database/mongo/index.ts
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 
-let connection = null;
 
 export default class MongoClient {
-  static connect = () => {
-    var mongoDb = "mongodb://root:root@localhost:27018";
-    mongoose.connect(mongoDb);
+  static connect = () => {    
+    var mongoDb = "mongodb://root:root@mongo:27017";
+    mongoose.connect(mongoDb)
     var connection = mongoose.connection;
+
+    
     connection.on("error", console.error.bind(console, "MongoDB Connection error"));
+
+    connection.once("open", () => {
+      console.log("MongoDB connected successfully");
+    });
+
+    
+    connection.on("disconnected", () => {
+      console.log("MongoDB disconnected");
+    });
   }
 }
